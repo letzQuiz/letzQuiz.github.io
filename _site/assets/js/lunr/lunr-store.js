f5b5ca2f9464bdeaafab72057cf414bd0f1b8bf8
ErikPillon <erik.pillon@gmail.com>
2024-07-01 22:39:11 +0200

    fix: :bug: add github page to gemfile

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Fun With Flags",
         "excerpt":"Erik Pillon presents: Fun with Flags The following questions were asked during the special round “Erik Pillon presents: Fun with Flags” of the pubquiz on the 9th of September, 2022. Question: How many countries in the world have square flags? Answer: Switzerland and Vatican City Question: There are only 3...","categories": ["Questions"],
         "tags": ["fun-with-flags"],
-        "url": "/questions/Fun-With-Flags/",
+        "url": "/questions/fun-with-flags/",
         "teaser": null
       },{
         "title": "Welcome to Letz Quiz!",
         "excerpt":"Welcome   Hello world, this is my first blog post.   I hope you like it!  ","categories": ["Posts"],
         "tags": ["welcome"],
-        "url": "/posts/Lets-Quiz/",
+        "url": "/posts/letz-quiz/",
         "teaser": null
       }]
