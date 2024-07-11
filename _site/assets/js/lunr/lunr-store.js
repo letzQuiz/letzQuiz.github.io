290eb847b4063ca958ef073646c2bbbef4627d5b
ErikPillon <erik.pillon@gmail.com>
2024-07-11 23:26:53 +0200

    moved the page structure into the layout folder

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": null
       },{
         "title": "The Vengeance of the Quizmaster",
-        "excerpt":"General Knowledge Question: July 6th, 1885 is the anniversary of the first injection of the Rabies vaccine. Which famous scientist was responsible for this invention? Answer: Louis Pasteur Question: During the celebrations of the last national day, Grand Duke Henry surprisingly announced that he started the abdication process of Luxembourg....","categories": ["Questions"],
+        "excerpt":" ","categories": ["Questions"],
         "tags": [],
         "url": "/questions/the-vengeance-of-the-quizmaster/",
         "teaser": null
